55913a1a886ce52107ec9387518ec2a5e9a001a7
mohammedchakir <zerroukimedchakir@gmail.com>
2023-10-05 07:58:23 -0700

    correct task 12

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
+from magic_calculation_102 import add, sub
+
     if a < b:
-        add = __import__('magic_calculation_102').add
         c = add(a, b)
-        for i in range(4, 7):
+        for i in range(4, 6):
             c = add(c, i)
         return (c)
     else:
-        sub = __import__('magic_calculation_102').sub
         return sub(a, b)
