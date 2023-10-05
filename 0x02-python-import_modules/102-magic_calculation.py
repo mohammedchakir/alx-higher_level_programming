d026cbe766d7f47797e8d6c30e14f6b753256bba
mohammedchakir <zerroukimedchakir@gmail.com>
2023-10-05 08:07:00 -0700

    correct task

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
+
 def magic_calculation(a, b):
-from magic_calculation_102 import add, sub
+    from magic_calculation_102 import add, sub
 
     if a < b:
         c = add(a, b)
@@ -8,4 +9,4 @@ from magic_calculation_102 import add, sub
             c = add(c, i)
         return (c)
     else:
-        return sub(a, b)
+        return(sub(a, b))
