67056e2050425a8478b1f4588411bed1318aa80d
mohammedchakir <zerroukimedchakir@gmail.com>
2023-10-23 09:26:27 -0700

    correct task5

--- 0x05-python-exceptions/5-raise_exception.py
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
+
 def raise_exception():
-    try:
-        raise TypeError("Exception raised")
-    except TypeError as te:
-        print("Exception raised")
+    """
+    This function raises a TypeError.
+    """
+    raise TypeError("Exception raised")
