bfb7a7e990f39224cd939482120c6c60f3519bd1
mohammedchakir <zerroukimedchakir@gmail.com>
2023-10-23 09:42:33 -0700

    correct task9

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -7,9 +7,8 @@ def magic_calculation(a, b):
             if i > a:
                 raise Exception("Too far")
             result += (a ** b) / i
-        except:
+        except Exception:
             result = b + a
             break
 
     return result
-
