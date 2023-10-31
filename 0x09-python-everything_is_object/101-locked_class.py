af4e748b400a56e169f3132942a0a8b321756941
mohammedchakir <zerroukimedchakir@gmail.com>
2023-10-31 07:17:23 -0700

    correct a task

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
+
+"""defines a locked class."""
+
+
 class LockedClass:
+    """
+    This is a locked class with a 'first_name' attribute.
+    """
     __slots__ = 'first_name'
