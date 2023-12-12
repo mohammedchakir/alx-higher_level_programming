7e06d938a016edbd16148df78ca7a33b2a0fb752
mohammedchakir <zerroukimedchakir@gmail.com>
2023-12-12 14:03:08 -0800

    last push

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,7 +1,4 @@
 -- This SQL script displays the top 3 cities by average temperature during July and August, ordered by temperature in descending order.
-SELECT city, AVG(temperature) AS avg_temp
-FROM temperature_data -- Replace 'temperature_data' with the actual table name
-WHERE MONTH(date_column) IN (7, 8) -- Replace 'date_column' with the actual date column name
-GROUP BY city
-ORDER BY avg_temp DESC
-LIMIT 3;
+USE hbtn_0c_0;
+
+SELECT city, AVG(value) AS avg_temp FROM temperatures WHERE MONTH=7 OR MONTH=8 GROUP BY city ORDER BY avg_temp DESC LIMIT 3;
--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,5 +1,3 @@
 -- This SQL script displays the maximum temperature of each state, ordered by state name.
-SELECT state, MAX(temperature) AS max_temp
-FROM temperature_data -- Replace 'temperature_data' with the actual table name
-GROUP BY state
-ORDER BY state;
+USE hbtn_0c_0;
+SELECT state, MAX(value) AS max_temp FROM temperatures GROUP BY state ORDER BY state;
