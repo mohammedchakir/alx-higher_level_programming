3468ad1d726651b4ca5eff2ef83d9da491e34ca8
mohammedchakir <zerroukimedchakir@gmail.com>
2023-12-12 12:51:21 -0800

    testing again

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,6 +1,3 @@
 -- SQL script to calculate and display the average temperature by city
 -- This script should be executed in the MySQL environment
-SELECT city, AVG(value) AS avg_temp
-FROM temperatures
-GROUP BY city
-ORDER BY avg_temp DESC;
+SELECT city, AVG(value) AS avg_temp FROM temperatures GROUP BY city ORDER BY avg_temp DESC;
