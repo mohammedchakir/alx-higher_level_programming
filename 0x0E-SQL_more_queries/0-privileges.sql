c58f88b9b4badcb71d6d110585aef7e6b8145a76
mohammedchakir <zerroukimedchakir@gmail.com>
2023-12-13 04:07:52 -0800

    commiting the fist task

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,4 @@
+-- This script lists all global privileges for MySQL users 'user_0d_1' and 'user_0d_2' on localhost
+-- Show grants for user 'user_0d_1', Show grants for user 'user_0d_2'
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
