2e84f12eb5573b83cbddc2ad1788da2af5961efd
mohammedchakir <zerroukimedchakir@gmail.com>
2024-01-07 17:57:40 +0100

    append new files

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-This script lists all states from the database hbtn_0e_0_usa.
+This script lists all cities from the database hbtn_0e_4_usa.
 It takes three arguments: MySQL username, MySQL password, and database name.
 """
 
@@ -8,14 +8,14 @@ import MySQLdb
 import sys
 
 
-def list_states(username, password, db_name):
+def list_cities(username, password, db_name):
     """
-    Lists all states from the database.
+    Lists all cities from the database in ascending order by cities.id.
     """
     db = MySQLdb.connect(host="localhost", port=3306, user=username,
                          passwd=password, db=db_name)
     cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    cur.execute("SELECT * FROM cities ORDER BY id ASC")
     rows = cur.fetchall()
     for row in rows:
         print(row)
@@ -25,4 +25,4 @@ def list_states(username, password, db_name):
 
 if __name__ == "__main__":
     if len(sys.argv) == 4:
-        list_states(sys.argv[1], sys.argv[2], sys.argv[3])
+        list_cities(sys.argv[1], sys.argv[2], sys.argv[3])
