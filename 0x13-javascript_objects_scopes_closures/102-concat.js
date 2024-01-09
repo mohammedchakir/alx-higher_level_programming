e298851116244bcfaa8b0237e909ea0c54e3bc0c
mohammedchakir <zerroukimedchakir@gmail.com>
2024-01-09 13:42:38 +0100

    testing

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -2,11 +2,24 @@
 
 const fs = require('fs');
 
+if (process.argv.length !== 5) {
+  console.error('Usage: ./102-concat.js <fileA> <fileB> <fileC>');
+  process.exit(1);
+}
+
 const fileA = process.argv[2];
 const fileB = process.argv[3];
 const fileC = process.argv[4];
 
-const contentA = fs.readFileSync(fileA, 'utf8');
-const contentB = fs.readFileSync(fileB, 'utf8');
+try {
+  const contentA = fs.readFileSync(fileA, 'utf8');
+  const contentB = fs.readFileSync(fileB, 'utf8');
+  const combinedContent = contentA + contentB;
 
-fs.writeFileSync(fileC, contentA + '\n' + contentB);
+  fs.writeFileSync(fileC, combinedContent);
+
+  console.log(`Contents of ${fileA} and ${fileB} have been concatenated to ${fileC}`);
+} catch (error) {
+  console.error('An error occurred:', error.message);
+  process.exit(1);
+}
