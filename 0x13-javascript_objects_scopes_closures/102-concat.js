1558b9071a30ef1252ce411cb256e066ba78e809
mohammedchakir <zerroukimedchakir@gmail.com>
2024-01-09 05:57:59 -0800

    testing

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -7,12 +7,12 @@ const fileB = process.argv[3];
 const fileC = process.argv[4];
 
 fs.readFile(fileA, 'utf8', (err, fileA) => {
+  if (err) throw err;
+  fs.readFile(fileB, 'utf8', (err, fileB) => {
     if (err) throw err;
-    fs.readFile(fileB, 'utf8', (err, fileB) => {
-        if (err) throw err;
-        const content = fileA + fileB;
-        fs.writeFile(fileC, content, (err) => {
-            if (err) throw err;
-        });
+    const content = fileA + fileB;
+    fs.writeFile(fileC, content, (err) => {
+      if (err) throw err;
     });
+  });
 });
