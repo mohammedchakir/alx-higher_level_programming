81cfc7e54ff4f8f56718e0f96b5eed59d3bed494
mohammedchakir <zerroukimedchakir@gmail.com>
2024-01-09 00:40:04 -0800

    testing

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -5,10 +5,10 @@ const { dict } = require('./101-data.js');
 const idsByOccurrence = {};
 
 for (const [userId, occurrence] of Object.entries(dict)) {
-    if (!idsByOccurrence[occurrence]) {
-        idsByOccurrence[occurrence] = [];
-    }
-    idsByOccurrence[occurrence].push(userId);
+  if (!idsByOccurrence[occurrence]) {
+    idsByOccurrence[occurrence] = [];
+  }
+  idsByOccurrence[occurrence].push(userId);
 }
 
 console.log(idsByOccurrence);
