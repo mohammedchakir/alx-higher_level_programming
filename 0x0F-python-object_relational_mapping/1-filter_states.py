775900cd630063e8c5445b009694a84c94bf757a
mohammedchakir <zerroukimedchakir@gmail.com>
2024-01-11 13:35:24 +0100

    adding files

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,31 +1,22 @@
 #!/usr/bin/python3
-"""
-A script that lists all states with names starting with 'N'
-from the database hbtn_0e_0_usa.
-"""
+"""This script lists all states from the database `hbtn_0e_0_usa`"""
 
 import MySQLdb
 import sys
 
-
-def list_states_starting_with_n(username, password, db_name):
-    """
-    Connects to a MySQL database and prints all states starting
-    with 'N', sorted by id.
-    """
-    db = MySQLdb.connect(
-        host="localhost", port=3306, user=username, passwd=password, db=db_name)
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
-
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
-
-    cur.close()
-    db.close()
-
-
 if __name__ == "__main__":
-    if len(sys.argv) == 4:
-        list_states_starting_with_n(sys.argv[1], sys.argv[2], sys.argv[3])
+    
+
+    db = MySQLdb.connect(
+        host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cursor = db.cursor()
+    query = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC"
+    
+    cursor.execute(query)
+    states = cursor.fetchall()
+    
+    for state in states:
+        print(state)
+        
+        cursor.close()
+        db.close()
--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,30 +1,22 @@
 #!/usr/bin/python3
-"""
-A script that displays all values in the states table of hbtn_0e_0_usa
-where name matches the argument.
-"""
+"""This script lists all states from the database `hbtn_0e_0_usa`"""
 
 import MySQLdb
 import sys
 
-def search_state(username, password, db_name, state_name):
-    """
-    Connects to a MySQL database and prints states
-    where name matches the given argument.
-    """
-    db = MySQLdb.connect(
-        host="localhost", port=3306, user=username, passwd=password, db=db_name)
-    cur = db.cursor()
-    query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
-    cur.execute(query, (state_name,))
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
-
-    cur.close()
-    db.close()
-
-
 if __name__ == "__main__":
-    if len(sys.argv) == 5:
-        search_state(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
+    
+
+    db = MySQLdb.connect(
+        host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cursor = db.cursor()
+    query = "SELECT * FROM states WHERE name = %s ORDER BY states.id ASC"
+    
+    cursor.execute(query)
+    states = cursor.fetchall()
+    
+    for state in states:
+        print(state)
+        
+        cursor.close()
+        db.close()
