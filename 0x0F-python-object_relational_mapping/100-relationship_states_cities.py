6c013c1ec488a8d6525d15de1bb591e343031557
mohammedchakir <zerroukimedchakir@gmail.com>
2024-01-12 12:35:28 -0800

    appending changes

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -14,16 +14,16 @@ from relationship_city import City
 if __name__ == "__main__":
 
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
-        sys.argv[1], sys.argv[2], sys.argv[3]))
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
 
     session = Session()
     new_state = State(name='California')
     new_city = City(name='San Francisco')
+    new_state.cities.append(new_city)
 
     session.add(new_state)
-    session.add(new_city)
     session.commit()
-
     session.close()
