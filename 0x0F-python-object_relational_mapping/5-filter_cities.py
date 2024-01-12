38802be43520bee452eef3933f66f09da5e6c1b1
mohammedchakir <zerroukimedchakir@gmail.com>
2024-01-11 21:54:27 -0800

    testing

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
 
     query = ("SELECT cities.id, cities.name FROM cities JOIN states \
             ON cities.state_id = states.id WHERE states.name \
-            LIKE BINARY %(state_name)s ORDER BY cities.id ASC", (sys.argv[4],))
+            LIKE BINARY %s ORDER BY cities.id ASC", (sys.argv[4],))
 
     cursor.execute(query)
     results = cursor.fetchall()
