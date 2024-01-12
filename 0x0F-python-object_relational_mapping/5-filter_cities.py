9d9a77e6a1b63163972511f473c2a4c388efe181
mohammedchakir <zerroukimedchakir@gmail.com>
2024-01-13 00:39:58 +0100

    test 5

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -7,24 +7,26 @@ import MySQLdb
 
 if __name__ == "__main__":
 
+    state_name = sys.argv[4]
+    query = """
+    SELECT cities.id, cities.name
+    FROM cities
+    INNER JOIN states ON cities.state_id = states.id
+    WHERE states.name %(state_name)s
+    ORDER BY cities.id ASC
+    """
+    trans = (state_name,)
+
     db = MySQLdb.connect(
         user=sys.argv[1],
         passwd=sys.argv[2],
         host="localhost",
         port=3306,
-        db=sys.argv[3],
-        charset="utf8"
-    )
+        db=sys.argv[3]
+        )
     cursor = db.cursor()
-    query = """
-    SELECT cities.id, cities.name
-    FROM cities
-    INNER JOIN states ON cities.state_id = states.id
-    WHERE states.name = %s
-    ORDER BY cities.id ASC
-    """
-    cursor.execute(query, (sys.argv[4],))
 
+    cursor.execute(query, trans)
     cities = cursor.fetchall()
 
     for city in cities:
