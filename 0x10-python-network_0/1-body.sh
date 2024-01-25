7af9773e39deb55b2c45b8bca39de9bd748e4e82
mohammed chakir <129831433+mohammedchakir@users.noreply.github.com>
2024-01-25 20:01:13 +0100

    Update 1-body.sh

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
 # This script takes a URL, sends a GET request, and displays the body of the response for a 200 status code
-
-url=$1
-response=$(curl -s -w "%{http_code}" "$url")
-
-# Check if the status code is 200
-if [[ $response == *"200"* ]]; then
-    body=$(curl -s "$url")
-    echo "$body"
-fi
-
+curl -sL "$1"
