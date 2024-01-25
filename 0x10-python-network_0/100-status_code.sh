16e4d26ab30b7b4889d67095b34c0ed55e6511d2
mohammed chakir <129831433+mohammedchakir@users.noreply.github.com>
2024-01-25 20:43:40 +0100

    Update 100-status_code.sh

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 # This script sends a request to a URL and displays only the status code of the response
-
-url=$1
-curl -s -o /dev/null -w "%{http_code}" "$url"
-
+curl -s -o /dev/null -w "%{http_code}" "$1"
