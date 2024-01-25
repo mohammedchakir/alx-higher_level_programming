a87bda67b7fe795e31e5f844866bfdc043b736bb
mohammed chakir <129831433+mohammedchakir@users.noreply.github.com>
2024-01-25 20:45:44 +0100

    Update 101-post_json.sh

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # This script sends a JSON POST request to a URL and displays the body of the response
-# Send POST request with JSON content
 curl -s -H "Content-Type: application/json" -d "$(cat "$2")" "$1"
