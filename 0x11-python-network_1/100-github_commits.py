aa5dd6ae8a1a313d25a45fdc738e9a40db1bc242
mohammed chakir <129831433+mohammedchakir@users.noreply.github.com>
2024-01-26 19:35:49 +0100

    Update 100-github_commits.py

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -9,10 +9,7 @@ import sys
 
 
 if __name__ == "__main__":
-    repository_name = sys.argv[1]
-    owner_name = sys.argv[2]
-
-    url = "https://api.github.com/repos/{owner_name}/{repository_name}/commits"
+    url = "https://api.github.com/repos/{}/{}/commits".format(sys.argv[1], sys.argv[2])
     response = requests.get(url)
     commits = response.json()
     try:
