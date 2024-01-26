4fbad134f1373c7ff1223a0fef61aa2e15958fec
mohammed chakir <129831433+mohammedchakir@users.noreply.github.com>
2024-01-26 16:10:17 +0100

    Update 2-post_email.py

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -10,13 +10,11 @@ import urllib.request
 import urllib.parse
 import sys
 
-if len(sys.argv) != 3:
-    sys.exit("Usage: ./2-post_email.py <URL> <email>")
 
-url = sys.argv[1]
-email = sys.argv[2]
-
-data = urllib.parse.urlencode({'email': email}).encode('utf-8')
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+    data = urllib.parse.urlencode({'email': email}).encode('utf-8')
 
 with urllib.request.urlopen(url, data=data) as response:
     content = response.read().decode('utf-8')
