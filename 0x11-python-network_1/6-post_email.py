fbe92672a27264160f50d68c74032a0888106693
mohammed chakir <129831433+mohammedchakir@users.noreply.github.com>
2024-01-26 16:32:21 +0100

    Update 6-post_email.py

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -9,13 +9,10 @@ the requests package, and displays the body of the response.
 import requests
 import sys
 
-if len(sys.argv) != 3:
-    sys.exit("Usage: ./6-post_email.py <URL> <email>")
 
-url = sys.argv[1]
-email = sys.argv[2]
-
-data = {'email': email}
-response = requests.post(url, data=data)
-
-print("Your email is:", response.text)
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+    data = {'email': email}
+    response = requests.post(url, data=data)
+    print(response.text)
