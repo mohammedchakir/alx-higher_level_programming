7b9ffdbf45ad5d157379503b20cf332010fe3ebb
mohammed chakir <129831433+mohammedchakir@users.noreply.github.com>
2024-01-26 16:45:20 +0100

    Update 8-json_api.py

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -5,19 +5,16 @@ This script takes a letter as a command-line argument, sends a POST request to
 http://0.0.0.0:5000/search_user with the letter as a parameter using the
 requests package, and processes the JSON response accordingly.
 """
-
 import requests
 import sys
 
-if len(sys.argv) < 2:
-    q = ""
-else:
-    q = sys.argv[1]
 
-url = 'http://0.0.0.0:5000/search_user'
-data = {'q': q}
-
-response = requests.post(url, data=data)
+if __name__ == "__main__":
+    url = 'http://0.0.0.0:5000/search_user'
+    lett = "" if len(sys.argv) == 1 else sys.argv[1]
+    data = {"q": lett}
+    
+    response = requests.post(url, data=data)
 
 try:
     json_data = response.json()
