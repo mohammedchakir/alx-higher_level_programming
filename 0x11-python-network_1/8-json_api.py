f2082fc323a7dbdfc583f610ffed5c65a60c4f99
mohammedchakir <zerroukimedchakir@gmail.com>
2024-01-26 07:46:34 -0800

    adding

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     url = 'http://0.0.0.0:5000/search_user'
     lett = "" if len(sys.argv) == 1 else sys.argv[1]
     data = {"q": lett}
-    
+
     response = requests.post(url, data=data)
 
 try:
