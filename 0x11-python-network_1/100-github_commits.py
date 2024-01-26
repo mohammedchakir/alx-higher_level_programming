8742e53c3fdebfeb3caf27d2871d2fffd646bee7
mohammed chakir <129831433+mohammedchakir@users.noreply.github.com>
2024-01-26 19:28:36 +0100

    Update 100-github_commits.py

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,27 +1,23 @@
 #!/usr/bin/python3
-
 """
 This script takes the repository name and owner name as command-line arguments,
 uses the GitHub API to retrieve the 10 most recent commits from the specified
 repository, and prints them in the required format.
 """
-
 import requests
 import sys
 
-if len(sys.argv) != 3:
-    sys.exit("Usage: ./100-github_commits.py <repository_name> <owner_name>")
 
-repository_name = sys.argv[1]
-owner_name = sys.argv[2]
-
-url = f'https://api.github.com/repos/{owner_name}/{repository_name}/commits'
-params = {'per_page': 10}
-response = requests.get(url, params=params)
-
-commits = response.json()
-
-for commit in commits:
-    sha = commit.get('sha')
-    author_name = commit.get('commit').get('author').get('name')
-    print(f'{sha}: {author_name}')
+if __name__ == "__main__":
+    repository_name = sys.argv[1]
+    owner_name = sys.argv[2]
+    
+    url = "https://api.github.com/repos/{owner_name}/{repository_name}/commits"
+    response = requests.get(url)
+    commits = response.json()
+    try:
+        for i in range(10):
+            print("{}: {}".format(commits[i].get("sha"),
+                                  commits[i].get("commit").get("author").get("name")))
+    except IndexError:
+        pass
