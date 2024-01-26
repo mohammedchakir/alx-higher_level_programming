69d0f245b47d87fe4c7860062198c207b483c9c3
mohammedchakir <zerroukimedchakir@gmail.com>
2024-01-26 07:28:56 -0800

    adding solutions

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -10,9 +10,8 @@ import sys
 
 
 if __name__ == "__main__":
-url = sys.argv[1]
+    url = sys.argv[1]
+    response = requests.get(url)
 
-response = requests.get(url)
-
-x_request_id = response.headers.get('X-Request-Id')
-print(x_request_id)
+    x_request_id = response.headers.get('X-Request-Id')
+    print(x_request_id)
