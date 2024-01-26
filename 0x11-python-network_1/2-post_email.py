8b0ac788d9ace71e90b13af10af7a80f6c4827d3
mohammedchakir <zerroukimedchakir@gmail.com>
2024-01-26 07:14:44 -0800

    addings

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -16,6 +16,6 @@ if __name__ == "__main__":
     value = {"email": sys.argv[2]}
     data = urllib.parse.urlencode(value).encode("ascii")
 
- request = urllib.request.Request(url, data)
+    request = urllib.request.Request(url, data)
     with urllib.request.urlopen(request) as response:
         print(response.read().decode("utf-8"))
