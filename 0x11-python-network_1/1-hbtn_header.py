66261cf3d44a2244ed241077bd45b9243a386f37
mohammed chakir <129831433+mohammedchakir@users.noreply.github.com>
2024-01-26 16:01:01 +0100

    Update 1-hbtn_header.py

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -10,10 +10,9 @@ in the header of the response.
 import urllib.request
 import sys
 
-if len(sys.argv) != 2:
-    sys.exit("Usage: ./1-hbtn_header.py <URL>")
 
-url = sys.argv[1]
+if __name__ == "__main__":
+    url = sys.argv[1]
 
 with urllib.request.urlopen(url) as response:
     x_request_id = response.headers.get('X-Request-Id')
