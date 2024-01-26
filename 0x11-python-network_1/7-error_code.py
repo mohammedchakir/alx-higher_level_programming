cad05e8bc406b0b63d790797e40e073808f55002
mohammed chakir <129831433+mohammedchakir@users.noreply.github.com>
2024-01-26 16:35:30 +0100

    Update 7-error_code.py

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -6,16 +6,13 @@ specified URL using the requests package, and displays the body of the
 response. If the HTTP status code is greater than or equal to 400,
 it prints an error message with the value of the HTTP status code.
 """
-
 import requests
 import sys
 
-if len(sys.argv) != 2:
-    sys.exit("Usage: ./7-error_code.py <URL>")
 
-url = sys.argv[1]
-
-response = requests.get(url)
+if __name__ == "__main__":
+    url = sys.argv[1]
+    response = requests.get(url)
 
 if response.status_code >= 400:
     print("Error code:", response.status_code)
