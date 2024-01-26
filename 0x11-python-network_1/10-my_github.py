93c6bba89c219f5495ddc0e5cbba95115690c63f
mohammedchakir <zerroukimedchakir@gmail.com>
2024-01-26 10:11:17 -0800

    add

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,18 +1,31 @@
 #!/usr/bin/python3
-
 """
-This script takes GitHub credentials (username and personal access token) as
-command-line arguments, uses Basic Authentication with the GitHub API to
-display the user's ID.
+Uses the GitHub API to retrieve and display a GitHub user ID based on given credentials.
+Usage: ./github_user_id.py <GitHub username> <GitHub password>
+  - Utilizes Basic Authentication to access the user ID.
 """
-from requests.auth import HTTPBasicAuth
-import requests
 import sys
+import requests
 
+def get_github_user_id(username, password):
+    url = "https://api.github.com/user"
+    response = requests.get(url, auth=(username, password))
+
+    if response.status_code == 200:
+        return response.json().get("id")
+    else:
+        print(f"Error: {response.status_code}")
+        return None
 
 if __name__ == "__main__":
-    auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
-    
-    url = 'https://api.github.com/user'
-    response = requests.get(url, auth=auth)
-    print(r.json().get("id"))
+    if len(sys.argv) != 3:
+        print("Usage: ./github_user_id.py <GitHub username> <GitHub password>")
+    else:
+        github_username, github_password = sys.argv[1], sys.argv[2]
+        user_id = get_github_user_id(github_username, github_password)
+
+        if user_id is not None:
+            print(f"GitHub User ID: {user_id}")
+        else:
+            print("Failed to retrieve GitHub User ID.")
+
