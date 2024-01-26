3427ad53db8c0cc6b358e3aac958db3ddaa92f05
mohammedchakir <zerroukimedchakir@gmail.com>
2024-01-26 10:31:21 -0800

    add

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -11,13 +11,14 @@ import sys
 if __name__ == "__main__":
     repository_name = sys.argv[1]
     owner_name = sys.argv[2]
-    
+
     url = "https://api.github.com/repos/{owner_name}/{repository_name}/commits"
     response = requests.get(url)
     commits = response.json()
     try:
         for i in range(10):
-            print("{}: {}".format(commits[i].get("sha"),
-                                  commits[i].get("commit").get("author").get("name")))
+            print("{}: {}".format(
+                commits[i].get("sha"),
+                commits[i].get("commit").get("author").get("name")))
     except IndexError:
         pass
