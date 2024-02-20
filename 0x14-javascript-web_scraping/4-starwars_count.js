f8fbad4cdf57cf455c8e9a728b85d30d64a9716a
mohammed chakir <129831433+mohammedchakir@users.noreply.github.com>
2024-02-20 17:22:30 +0100

    Update 4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,13 +1,12 @@
 #!/usr/bin/node
 
 const request = require('request');
-
 const apiUrl = process.argv[2];
 const characterId = 18;
 
 request.get(apiUrl, (error, response, body) => {
   if (error) {
-    console.error(error);
+    console.log(error);
     return;
   }
 
@@ -16,6 +15,5 @@ request.get(apiUrl, (error, response, body) => {
     film.characters.includes(
       `https://swapi-api.alx-tools.com/api/people/${characterId}/`)
   );
-
   console.log(moviesWithWedgeAntilles.length);
 });
