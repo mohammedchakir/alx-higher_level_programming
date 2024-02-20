9e1ac56a509ab094b9ef592698b67b8c71a50819
mohammed chakir <129831433+mohammedchakir@users.noreply.github.com>
2024-02-20 22:47:31 +0100

    Update 3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
 
 const request = require('request');
-
 const movieId = process.argv[2];
 const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 
@@ -10,7 +9,6 @@ request.get(apiUrl, (error, response, body) => {
     console.error(error);
     return;
   }
-
   const movie = JSON.parse(body);
   console.log(movie.title);
 });
