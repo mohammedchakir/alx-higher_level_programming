9d9166cb662248784562cf4998a51686bcc57889
mohammedchakir <zerroukimedchakir@gmail.com>
2024-02-20 08:08:51 -0800

    adding solutions

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -10,5 +10,5 @@ fs.writeFile(filePath, content, 'utf-8', (err) => {
     console.error(err);
     return;
   }
-  console.log(`Content has been written to ${filePath}`);
+  console.log(`${content} has been written to ${filePath}`);
 });
