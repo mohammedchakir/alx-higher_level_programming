6623408d0d579f00c8e3f197d6b92d1c3b4bd140
mohammedchakir <zerroukimedchakir@gmail.com>
2024-02-20 07:42:50 -0800

    add

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+
+const request = require('request');
+const fs = require('fs');
+
+const url = process.argv[2];
+const filePath = process.argv[3];
+
+request.get(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+    return;
+  }
+
+  fs.writeFile(filePath, body, 'utf-8', (err) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log(`Content has been written to ${filePath}`);
+  });
+});
