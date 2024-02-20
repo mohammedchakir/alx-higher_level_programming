9ceee9a5043841d67016f6442f3cf4530e80674b
mohammed chakir <129831433+mohammedchakir@users.noreply.github.com>
2024-02-20 17:17:51 +0100

    Update 1-writeme.js

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,14 +1,11 @@
 #!/usr/bin/node
 
 const fs = require('fs');
-
 const filePath = process.argv[2];
 const content = process.argv[3];
 
 fs.writeFile(filePath, content, 'utf-8', (err) => {
   if (err) {
-    console.error(err);
-    return;
+    console.log(err);
   }
-  console.log(`${content} has been written to ${filePath}`);
 });
