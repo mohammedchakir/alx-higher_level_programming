cb64f5adcd46dc982ec1f135fc9c57597ad162ef
mohammedchakir <zerroukimedchakir@gmail.com>
2024-02-20 07:45:10 -0800

    add

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const apiUrl = process.argv[2];
+
+request.get(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error(error);
+    return;
+  }
+
+  const todos = JSON.parse(body);
+  const completedTasksByUser = {};
+
+  todos.forEach(todo => {
+    if (todo.completed) {
+      if (completedTasksByUser[todo.userId]) {
+        completedTasksByUser[todo.userId]++;
+      } else {
+        completedTasksByUser[todo.userId] = 1;
+      }
+    }
+  });
+
+  console.log(completedTasksByUser);
+});
