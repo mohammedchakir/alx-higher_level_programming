f5708ebb77afdb2199c289b4dd8e31153c90cf16
mohammedchakir <zerroukimedchakir@gmail.com>
2024-02-20 07:53:32 -0800

    adding files advanced

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,33 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const movieId = process.argv[2];
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+request.get(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error(error);
+    return;
+  }
+
+  const movie = JSON.parse(body);
+  const charactersUrls = movie.characters;
+
+  charactersUrls.reduce((prev, characterUrl) => {
+    return prev.then(() => {
+      return new Promise((resolve, reject) => {
+        request.get(characterUrl, (error, response, body) => {
+          if (error) {
+            console.error(error);
+            reject(error);
+            return;
+          }
+          const character = JSON.parse(body);
+          console.log(character.name);
+          resolve();
+        });
+      });
+    });
+  }, Promise.resolve());
+});
